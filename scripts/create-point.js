bd24dd28c19fee8079f06d5e0826a594caf292ab
Renan Augusto <43729151+renanaugust@users.noreply.github.com>
2020-06-06 23:42:46 -0300

    bugs cidades corrigidos

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -26,13 +26,16 @@ function getCities(event) {
     stateInput.value = event.target.options[indexOfSelectedState].text
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
-
+    
+    citySelect.innerHTML = "<option value>Selecione a cidade</option>"
+    citySelect.disabled = true
+    
     fetch(url)
     .then ( res => res.json() )
     .then( cities => {
-
+        
         for( const city of cities) {
-            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
         }
 
         citySelect.disabled = false
